0d512230951a958f95f426d42f52db045eb07f83
Thrishala2006 <koduruthrishala@gmail.com>
2026-08-17 10:00:12 +0530

    Update dynamic_url.js

--- dynamic_url.js
+++ dynamic_url.js
@@ -1,9 +1,31 @@
-const express=require("express");
+const express=require('express');
 const app=express();
-app.get("/student/:id",(req,res)=>{
-    const id=req.params.id;
-    res.send("<h1>Student ID: "+id+"</h1>");
+const port=5000;
+app.get('/user/:id',(req,res)=>
+{
+    const user_id=req.params.id;
+    res.json(
+        {
+            name:"thrishala",
+            id:user_id
+        }
+    );
 });
-app.listen(3000,()=>{
-    console.log("Server running on http:localhost:3000");
+app.get('/flights/:from/:to',(req,res)=>
+{
+   const {from,to}=req.params;
+   res.send(`<h1>searching for flights departing from ${from} to ${to}<h1>`);
+})
+app.get('/search',(req,res)=>
+{
+     const cat=req.query.cat;
+     const sort=req.query.sort;
+     res.json({
+        message:"serching movies",
+        cat:cat||"none",
+        sort:sort||"none"
+     });
+});
+app.listen(port,()=>{
+    console.log(`your server is running at port http://localhost:${port}`);
 });
